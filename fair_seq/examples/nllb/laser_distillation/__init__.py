58047c569016020738ca8f0c5edc95de4a307214
TranNhiem <tvnhiemhmus@g.ncu.edu.tw>
2022-07-12 17:29:07 +0800

    configure_installation_translation_module

--- fair_seq/examples/nllb/laser_distillation/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .encoder_similarity import *  # noqa
-from .laser_distillation_task import *  # noqa
